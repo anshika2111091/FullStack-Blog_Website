344dd569bd1decbbd2e32f9edc6c0951dd755225
Anshikapuri <anshika.puri@revfin.in>
2025-07-30 09:48:10 +0530

    added-routes

---
+++ src/components/Image.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { IKImage } from "imagekitio-react";
+
+const Image = ({src,classname,w,h,alt}) => {
+  return (
+    <IKImage
+    urlEndpoint={import.meta.env.VITE_IK_URL_ENDPOINT}
+   path={src}
+      className={classname}
+      loading="lazy"
+      lqip={{active:true,quality:20}}
+      alt={alt}
+      width={w}
+      height={h}
+    />
+  );
+};
+
+export default Image;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,17 @@
 import React,{useState} from 'react'
+import { IKImage } from 'imagekitio-react';
+import Image from './Image';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     const [open,setOpen]=useState(false);
   return (
     <div className='w-full h-16 md:h-20 flex items-center justify-between'>
         {/* Logo */}
-        <div className='flex items-center gap-4 text-2xl font-bold'>
-            <img src="./logo.png" className='w-8 h-8' alt="logo" />
+        <Link to="/" className='flex items-center gap-4 text-2xl font-bold'>
+         <Image src="logo.png" alt="logo" w={32} h={32}/>
             <span>lamalog</span>
-        </div>
+        </Link>
 
         {/* Mobile Menu */}
         <div className='md:hidden'>
@@ -18,25 +21,25 @@ const Navbar = () => {
 {/* Mobile link list */}
       <div className={`w-full h-screen absolute gap-8 font-medium text-lg top-16 flex flex-col items-center justify-center transition-all ease-in-out ${open ? "-right-0":"-right-[100%]"}`}>
 
-            <a href='/'>Home</a>
-            <a href='/'>Trending</a>
-            <a href='/'>Most Popular</a>
-            <a href='/'>About</a>
-            <a href='/'>
+            <Link to='/'>Home</Link>
+            <Link to='/'>Trending</Link>
+            <Link to='/'>Most Popular</Link>
+            <Link to='/'>About</Link>
+            <Link to='/'>
             <button className='py-2 px-4 rounded-3xl bg-blue-800 text-white'>Login 👋</button>
-            </a>
+            </Link>
        
             </div>
         </div>
         {/* Desktop Menu */}
         <div className='hidden md:flex items-center gap-8 xl:gap-12 font-medium'>
-            <a href='/'>Home</a>
-            <a href='/'>Trending</a>
-            <a href='/'>Most Popular</a>
-            <a href='/'>About</a>
-            <a href='/'>
+            <Link to='/'>Home</Link>
+            <Link to='/'>Trending</Link>
+            <Link to='/'>Most Popular</Link>
+            <Link to='/'>About</Link>
+            <Link to='/'>
             <button className='py-2 px-4 rounded-3xl bg-blue-800 text-white'>Login 👋</button>
-            </a>
+            </Link>
         </div>
     </div>
   )
