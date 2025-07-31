6058f6c7b3000163967215fb87cd95e69b69e652
Anshikapuri <anshika.puri@revfin.in>
2025-07-31 13:05:54 +0530

    added-posts-page-filters

--- src/routes/PostListPage.jsx
+++ src/routes/PostListPage.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import PostList from "../components/PostList";
+import SideMenu from '../components/SideMenu';
 const PostListPage = () => {
+  const [open,setOpen]=useState(false);
   return (
     <div>
       <h1 className='mb-8 text-2xl'>Development Blogs</h1>
+      <button className='md:hidden bg-blue-800 text-sm text-white px-4 py-2 rounded-2xl mb-4' onClick={()=>setOpen((prev)=>!prev)}>{open ? "Close":"Filter or Search"}</button>
+      <div className='flex gap-8 flex-col-reverse md:flex-row'>
+        <div><PostList/></div>
+        <div className={`${open ? "block":"hidden"} md:block`}><SideMenu/></div>
+      </div>
     </div>
   )
 }
