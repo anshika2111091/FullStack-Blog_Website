0b96668322ded4d6dbebee368d7563a41eb9a3a3
Anshikapuri <anshika.puri@revfin.in>
2025-07-31 11:34:42 +0530

    added-comment-section

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Image from './Image'
 
 const Comment = () => {
   return (
-    <div>Comment</div>
+    <div className='p-4 bg-slate-50 rounded-xl '>
+        <div className='flex items-center gap-4'>
+            <Image src="userImg.jpeg" classname="w-10 h-10 rounded-full object-cover" w="40"/>
+            <span className='font-medium'>John Doe</span>
+            <span className='text-sm text-gray-500'>2 days ago</span>
+        </div>
+        <div className='mt-4'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, laudantium rem! Veniam ab vero pariatur nisi dignissimos quae sit unde non dolore dicta? Natus sapiente, unde asperiores architecto cumque quisquam!</p>
+        </div>
+    </div>
   )
 }
 
--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Comment from './Comment'
 
 const Comments = () => {
   return (
-    <div>Comments</div>
+    <div className='flex flex-col gap-8 mb-8 lg:w-3/5'>
+        <h1 className='text-xl text-gray-500 underline'>
+       Comments
+        </h1>
+        <div className='flex items-center justify-between gap-8 w-full'>
+            <textarea placeholder='Write a comment...' className='flex-1 p-4 rounded-xl'/>
+            <button className='px-4 py-3 text-white bg-blue-800 rounded-xl font-medium'>Send</button>
+        </div>
+        <Comment/>
+        <Comment/>
+        <Comment/>
+        <Comment/>
+        <Comment/>
+        <Comment/>
+        <Comment/>
+    </div>
   )
 }
 
