33eb9704ec5bab5719ab75584ca90ad67b9da913
Anshikapuri <anshika.puri@revfin.in>
2025-08-01 10:53:54 +0530

    post-backend

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,21 @@ import commentRoutes from "./routes/comment.route.js"
 import connectDB from "./lib/connectDB.js";
 
 const app=express();
+
 connectDB();
+app.use(express.json());
 app.use("/users",userRoutes);
 app.use("/posts",postRoutes);
 app.use("/comments",commentRoutes);
+
+app.use((error,req,res,next)=>{
+  res.status(error.status || 500);
+    res.json({
+        "message":error.message || "Something went wrong",
+        "status":error.status,
+        "stack":error.stack
+    })
+})
 
 
 app.listen(3000,()=>{
--- backend/lib/connectDB.js
+++ backend/lib/connectDB.js
@@ -1,13 +1,15 @@
-import mongoose, { connect } from "mongoose";
+import mongoose from "mongoose";
 
-const connectDB=async()=>{
-    try{
-    await  mongoose.connect("mongodb://localhost:27017/blog_application");
-      console.log("Mongodb is connected")
-    }
-    catch(err){
-        console.log(err);
-    }
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/blog_application", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB is connected");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+  }
+};
 
-}
 export default connectDB;
